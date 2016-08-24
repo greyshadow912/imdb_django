6ecd4881d6fac7e0948cd5b73840a7d85949fba1
au1994 <abhishek.upadhyay.cse12@iitbhu.ac.in>
2016-08-25 04:16:04 +0530

    updated settings.py

--- search_movies/settings.py
+++ search_movies/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sojyb=n_h432uukjjyjz9t7lj=!5%11l!doarxet%(^zzu2vcv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
